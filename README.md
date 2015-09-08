7c6a2745c0fe65680158c397ac9201a7ea04cf11
Guillaume EHRET <gui.ehret@gmail.com>
2015-09-08 08:19:49 +0200

    Add a test example in Jasmine

---
+++ README.md
@@ -0,0 +1,5 @@
+# quickytestjs
+
+![Dev-Mind](http://dev-mind.fr/logo/logo_1500.png)
+
+How to implement tests in a JS app in 15 minutes
--- src/test/service/ConferenceService.spec.js
+++ src/test/service/ConferenceService.spec.js
@@ -8,7 +8,12 @@ describe('ConferenceService', function(){
 
   //Sometimes it's usefull to exec statements before or after a test. For that use beforeEach or AfterEach
   beforeEach(function(){
+    //We construct a fake object because the real one don't exist
+    httpService = {
+      get : function(){}
+    };
 
+    service = new ConferenceService(httpService);
   });
 
   //You can stack several describe block to define sub suites. It's a good way to increase readability
@@ -16,12 +21,25 @@ describe('ConferenceService', function(){
 
     //You can write your individual tests
     it('should return all the conference', function(){
-      expect(true).toBeTruthy();
+      //spyOn helps you to create stub. If you want to trace how your object is used you can see
+      // jasmine.createSpyObj
+      spyOn(httpService, 'get').and.returnValue([{title : 'Mix-IT'}, {title : 'JugSummerCamp'}]);
+      expect(service.getAll().length).toBe(2);
     });
 
   });
 
   describe('getById', function(){
+    it('should return the conference with id 1', function(){
+      spyOn(httpService, 'get').and.returnValue({title : 'Mix-IT'});
+      expect(service.getById(1).title).toBe('Mix-IT');
+    });
 
+
+    it('should return no conference when id is unknown', function(){
+      spyOn(httpService, 'get').and.returnValue(undefined);
+      expect(service.getById(999)).toBeUndefined();
+    });
   });
 });
+
